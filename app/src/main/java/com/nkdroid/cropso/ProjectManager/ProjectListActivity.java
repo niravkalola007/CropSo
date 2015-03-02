f03f2d79a59df962cca486049462e504702a6747
Nirav <niravnvk@gmail.com>
2015-03-02 20:15:19 +0530

    s

--- app/src/main/java/com/nkdroid/cropso/ProjectManager/ProjectListActivity.java
+++ app/src/main/java/com/nkdroid/cropso/ProjectManager/ProjectListActivity.java
@@ -2,39 +2,40 @@ package com.nkdroid.cropso.ProjectManager;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.nkdroid.cropso.R;
 
 public class ProjectListActivity extends ActionBarActivity {
-
+    private Toolbar toolbar;
+    private ListView pm_project_list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_list);
+        setActionBar();
+        pm_project_list= (ListView) findViewById(R.id.pm_project_list);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_project_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void setActionBar(){
+        //Set ActionBar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            toolbar.setNavigationIcon(R.drawable.ic_launcher);
+            toolbar.setTitle("CropSo");
+            setSupportActionBar(toolbar);
         }
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
-        return super.onOptionsItemSelected(item);
     }
 }
